939f38bbcaef4d3faf59f394762766f10cd70c39
TioRou <robertogp29@gmail.com>
2024-12-26 12:33:20 +0100

    Se arregla el problema de overload en los formularios de persona y vehículo

--- lib/app/ui/pages/reparacion/form_persona_page.dart
+++ lib/app/ui/pages/reparacion/form_persona_page.dart
@@ -25,90 +25,97 @@ class FormPersonaPage extends StatelessWidget {
           key: personaCntrl.formKeyPerson,
           child: SizedBox(
             width: Get.mediaQuery.size.width * 0.9,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                AutoCompleteCustom(
-                  controller: personaCntrl.nifCntrl,
-                  title: 'NIF',
-                  optionsBuilder: (TextEditingValue textEditingValue) {
-                      if (textEditingValue.text.isEmpty) {
-                          personaCntrl.nifSeleccionado = RxBool(false);
-                          personaCntrl.nifCntrl.text = "";
-                          personaCntrl.nameCntrl.clear();
-                          personaCntrl.surName1Cntrl.clear();
-                          personaCntrl.surName2Cntrl.clear();
-                          personaCntrl.emailCntrl.clear();
-                          personaCntrl.tlfCntrl.clear();
+            child: CustomScrollView(
+              slivers: [
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                      AutoCompleteCustom(
+                        controller: personaCntrl.nifCntrl,
+                        title: 'NIF',
+                        optionsBuilder: (TextEditingValue textEditingValue) {
+                            if (textEditingValue.text.isEmpty) {
+                                personaCntrl.nifSeleccionado = RxBool(false);
+                                personaCntrl.nifCntrl.text = "";
+                                personaCntrl.nameCntrl.clear();
+                                personaCntrl.surName1Cntrl.clear();
+                                personaCntrl.surName2Cntrl.clear();
+                                personaCntrl.emailCntrl.clear();
+                                personaCntrl.tlfCntrl.clear();
 
-                          return const Iterable<String>.empty();
-                      }
+                                return const Iterable<String>.empty();
+                            }
 
-                      if (personaCntrl.clientService.clientes
-                          .where((cliente) => cliente.nif!.startsWith(textEditingValue.text))
-                          .map((cliente) => cliente.nif!).isEmpty) {
-                          personaCntrl.nifCntrl.text = textEditingValue.text;
-                      }
+                            if (personaCntrl.clientService.clientes
+                                .where((cliente) => cliente.nif!.startsWith(textEditingValue.text))
+                                .map((cliente) => cliente.nif!).isEmpty) {
+                                personaCntrl.nifCntrl.text = textEditingValue.text;
+                            }
 
-                      return personaCntrl.clientService.clientes
-                            .where((cliente) => cliente.nif!.startsWith(textEditingValue.text))
-                            .map((cliente) => cliente.nif!);
-                  },
-                  displayStringForOption: (String option) {
-                    return option;
-                  },
-                  onSelected: (String nifCliente) {
-                    Cliente cliente = personaCntrl.clientService.clientes.singleWhere((cliente) => cliente.nif == nifCliente);
-                    personaCntrl.nifSeleccionado = RxBool(true);
-                    personaCntrl.cliente = cliente;
-                    personaCntrl.nifCntrl.text = cliente.nif!;
-                    personaCntrl.nameCntrl.text = cliente.nombre!;
-                    personaCntrl.nameCntrl.text = cliente.nombre!;
-                    personaCntrl.surName1Cntrl.text = cliente.apellido1!;
-                    personaCntrl.surName2Cntrl.text = cliente.apellido2!;
-                    personaCntrl.tlfCntrl.text = cliente.telefono!;
-                    personaCntrl.emailCntrl.text = cliente.email!;
-                  },
-                  onSubmitted: (String value) {
-                    personaCntrl.nifCntrl.text = value;
-                  },
-              
+                            return personaCntrl.clientService.clientes
+                                  .where((cliente) => cliente.nif!.startsWith(textEditingValue.text))
+                                  .map((cliente) => cliente.nif!);
+                        },
+                        displayStringForOption: (String option) {
+                          return option;
+                        },
+                        onSelected: (String nifCliente) {
+                          Cliente cliente = personaCntrl.clientService.clientes.singleWhere((cliente) => cliente.nif == nifCliente);
+                          personaCntrl.nifSeleccionado = RxBool(true);
+                          personaCntrl.cliente = cliente;
+                          personaCntrl.nifCntrl.text = cliente.nif!;
+                          personaCntrl.nameCntrl.text = cliente.nombre!;
+                          personaCntrl.nameCntrl.text = cliente.nombre!;
+                          personaCntrl.surName1Cntrl.text = cliente.apellido1!;
+                          personaCntrl.surName2Cntrl.text = cliente.apellido2!;
+                          personaCntrl.tlfCntrl.text = cliente.telefono!;
+                          personaCntrl.emailCntrl.text = cliente.email!;
+                        },
+                        onSubmitted: (String value) {
+                          personaCntrl.nifCntrl.text = value;
+                        },
+
+                      ),
+                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                      TextFormFieldCustom(
+                          controller: personaCntrl.nameCntrl,
+                          hintText: 'Nombre',
+                          obscureText: false),
+                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                      TextFormFieldCustom(
+                          controller: personaCntrl.surName1Cntrl,
+                          hintText: 'Primer apellido',
+                          obscureText: false),
+                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                      TextFormFieldCustom(
+                          controller: personaCntrl.surName2Cntrl,
+                          hintText: 'Segundo apellido',
+                          obscureText: false),
+                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                      TextFormFieldCustom(
+                          controller: personaCntrl.emailCntrl,
+                          //validator: (value) => Helpers.validateEmail(value),
+                          hintText: 'Email',
+                          keyboardTypeEmail: TextInputType.emailAddress,
+                          obscureText: false),
+                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                      TextFormFieldCustom(
+                          controller: personaCntrl.tlfCntrl,
+                          hintText: 'Telefono',
+                          obscureText: false),
+                      SizedBox(height: Get.mediaQuery.size.height * 0.12),
+                      BtnLoad(
+                          onTap: () => personaCntrl.setDataPersona(),
+                          btnController: personaCntrl.btnCntlPerson,
+                          title: 'Continuar'),
+                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                    ],
+                  ),
                 ),
-                SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                TextFormFieldCustom(
-                    controller: personaCntrl.nameCntrl,
-                    hintText: 'Nombre',
-                    obscureText: false),
-                SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                TextFormFieldCustom(
-                    controller: personaCntrl.surName1Cntrl,
-                    hintText: 'Primer apellido',
-                    obscureText: false),
-                SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                TextFormFieldCustom(
-                    controller: personaCntrl.surName2Cntrl,
-                    hintText: 'Segundo apellido',
-                    obscureText: false),
-                SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                TextFormFieldCustom(
-                    controller: personaCntrl.emailCntrl,
-                    //validator: (value) => Helpers.validateEmail(value),
-                    hintText: 'Email',
-                    keyboardTypeEmail: TextInputType.emailAddress,
-                    obscureText: false),
-                SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                TextFormFieldCustom(
-                    controller: personaCntrl.tlfCntrl,
-                    hintText: 'Telefono',
-                    obscureText: false),
-                SizedBox(height: Get.mediaQuery.size.height * 0.12),
-                BtnLoad(
-                    onTap: () => personaCntrl.setDataPersona(),
-                    btnController: personaCntrl.btnCntlPerson,
-                    title: 'Continuar'),
-                SizedBox(height: Get.mediaQuery.size.height * 0.02),
-              ],
+              ]
             ),
           ),
         ),
--- lib/app/ui/pages/reparacion/form_vehiculo_page.dart
+++ lib/app/ui/pages/reparacion/form_vehiculo_page.dart
@@ -29,113 +29,121 @@ class FormVehiculoPage extends StatelessWidget {
             width: Get.mediaQuery.size.width * 0.9,
             child: Obx(() => formVehiculoCntrl.changedListBrand.value && formVehiculoCntrl.changedListColor.value
                 ? const Center(child: CircularProgressIndicator())
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                      TextFormFieldCustom(
-                          controller: formVehiculoCntrl.registrationCntrl,
-                          validator: (value) => Helpers.validateEmpty(value),
-                          hintText: 'Matricula',
-                          obscureText: false),
-                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                      AutoCompleteCustom(
-                        initValue: formVehiculoCntrl.valueBrandEditing.value,
-                        controller: formVehiculoCntrl.brandCntrl,
-                        onSelected: formVehiculoCntrl.handleBrandSelection,
-                        displayStringForOption: (String brand) => brand,
-                        optionsBuilder: (TextEditingValue textEditingValue) {
-                          if (textEditingValue.text.isEmpty) {
-                            return const Iterable<String>.empty();
-                          }
-                          return formVehiculoCntrl.listNameBrand.where((String option) {
-                            final text = textEditingValue.text.toLowerCase();
-                            final optionLower = option.toLowerCase();
-                            // Compara si la opción comienza con el texto ingresado
-                            return optionLower.startsWith(text);
-                          });
-                        },       
+                : CustomScrollView(
+                  slivers: [
+                    SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Expanded(child: Container()),
+                            SizedBox(height: Get.mediaQuery.size.height * 0.12),
+                            TextFormFieldCustom(
+                                controller: formVehiculoCntrl.registrationCntrl,
+                                validator: (value) => Helpers.validateEmpty(value),
+                                hintText: 'Matricula',
+                                obscureText: false),
+                            SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                            AutoCompleteCustom(
+                              initValue: formVehiculoCntrl.valueBrandEditing.value,
+                              controller: formVehiculoCntrl.brandCntrl,
+                              onSelected: formVehiculoCntrl.handleBrandSelection,
+                              displayStringForOption: (String brand) => brand,
+                              optionsBuilder: (TextEditingValue textEditingValue) {
+                                if (textEditingValue.text.isEmpty) {
+                                  return const Iterable<String>.empty();
+                                }
+                                return formVehiculoCntrl.listNameBrand.where((String option) {
+                                  final text = textEditingValue.text.toLowerCase();
+                                  final optionLower = option.toLowerCase();
+                                  // Compara si la opción comienza con el texto ingresado
+                                  return optionLower.startsWith(text);
+                                });
+                              },
 
-                        title: 'Marcas',
-                      ),
-                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                      AutoCompleteCustom(
-                        initValue: formVehiculoCntrl.valueModelEditing.value,
-                        controller: formVehiculoCntrl.modelCntrl,
-                        onSelected: formVehiculoCntrl.handleModelSelection,
-                        optionsBuilder: (TextEditingValue textEditingValue) {
-                          if (textEditingValue.text.isEmpty) {
-                            return const Iterable<String>.empty();
-                          }
-                          return formVehiculoCntrl.modelNameList.where((String option) {
-                            final text = textEditingValue.text.toLowerCase();
-                            final optionLower = option.toLowerCase();
-                            // Compara si la opción comienza con el texto ingresado
-                            return optionLower.startsWith(text);
-                          });
-                        },
-                        displayStringForOption: (String option) => option,
-                        title: 'Modelo',
-                      ),
-                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                      SizedBox(
-                        width: Get.mediaQuery.size.width * 0.9,
-                        height: Get.mediaQuery.size.height * 0.061,
-                        child: Card(
-                          color: Colors.white,
-                          shape: Border.all(color: Color.fromRGBO(180, 180, 180, 0.3)),
-                          margin: EdgeInsets.zero,
-                          child: DropdownButton<ColorVehiculo>(
-                            value: formVehiculoCntrl.selectedColor.value,
-                            hint: const Text('Selecciona un color'),
-                            icon: const Icon(Icons.arrow_drop_down),
-                            underline: Container(
-                              height: 0,
-                              color: Colors.transparent,
+                              title: 'Marcas',
                             ),
-                            onChanged: (ColorVehiculo? colorVehiculo) => formVehiculoCntrl.handleColorSelection(colorVehiculo!),
-                            items: formVehiculoCntrl.listColores.map<DropdownMenuItem<ColorVehiculo>>((colorInfo) {
-                              final color = Color.fromRGBO(
-                                int.parse(colorInfo.colorR!),
-                                int.parse(colorInfo.colorG!),
-                                int.parse(colorInfo.colorB!),
-                                1.0,
-                              );
-                              return DropdownMenuItem<ColorVehiculo>(
-                                value: colorInfo,
-                                child: SizedBox(
-                                  width: Get.mediaQuery.size.width * 0.82,
-                                  child: ListTile(
-                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                                    leading: Container(
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                          color: Colors.black38, // Color del borde
-                                          width: 1.0, // Grosor del borde
+                            SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                            AutoCompleteCustom(
+                              initValue: formVehiculoCntrl.valueModelEditing.value,
+                              controller: formVehiculoCntrl.modelCntrl,
+                              onSelected: formVehiculoCntrl.handleModelSelection,
+                              optionsBuilder: (TextEditingValue textEditingValue) {
+                                if (textEditingValue.text.isEmpty) {
+                                  return const Iterable<String>.empty();
+                                }
+                                return formVehiculoCntrl.modelNameList.where((String option) {
+                                  final text = textEditingValue.text.toLowerCase();
+                                  final optionLower = option.toLowerCase();
+                                  // Compara si la opción comienza con el texto ingresado
+                                  return optionLower.startsWith(text);
+                                });
+                              },
+                              displayStringForOption: (String option) => option,
+                              title: 'Modelo',
+                            ),
+                            SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                            SizedBox(
+                              width: Get.mediaQuery.size.width * 0.9,
+                              height: Get.mediaQuery.size.height * 0.061,
+                              child: Card(
+                                color: Colors.white,
+                                shape: Border.all(color: Color.fromRGBO(180, 180, 180, 0.3)),
+                                margin: EdgeInsets.zero,
+                                child: DropdownButton<ColorVehiculo>(
+                                  value: formVehiculoCntrl.selectedColor.value,
+                                  hint: const Text('Selecciona un color'),
+                                  icon: const Icon(Icons.arrow_drop_down),
+                                  underline: Container(
+                                    height: 0,
+                                    color: Colors.transparent,
+                                  ),
+                                  onChanged: (ColorVehiculo? colorVehiculo) => formVehiculoCntrl.handleColorSelection(colorVehiculo!),
+                                  items: formVehiculoCntrl.listColores.map<DropdownMenuItem<ColorVehiculo>>((colorInfo) {
+                                    final color = Color.fromRGBO(
+                                      int.parse(colorInfo.colorR!),
+                                      int.parse(colorInfo.colorG!),
+                                      int.parse(colorInfo.colorB!),
+                                      1.0,
+                                    );
+                                    return DropdownMenuItem<ColorVehiculo>(
+                                      value: colorInfo,
+                                      child: SizedBox(
+                                        width: Get.mediaQuery.size.width * 0.82,
+                                        child: ListTile(
+                                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                          leading: Container(
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                color: Colors.black38, // Color del borde
+                                                width: 1.0, // Grosor del borde
+                                              ),
+                                            ),
+                                            child: Icon(
+                                              Icons.square,
+                                              color: color,
+                                              size: 25.0,
+                                            ),
+                                          ),
+                                          title: Text(colorInfo.nombre!),
                                         ),
                                       ),
-                                      child: Icon(
-                                        Icons.square,
-                                        color: color,
-                                        size: 25.0,
-                                      ),
-                                    ),
-                                    title: Text(colorInfo.nombre!),
-                                  ),
+                                    );
+                                  }).toList(),
                                 ),
-                              );
-                            }).toList(),
-                          ),
+                              ),
+                            ),
+                            SizedBox(height: Get.mediaQuery.size.height * 0.12),
+                            BtnLoad(
+                                onTap: () => formVehiculoCntrl.setDataVehicle(),
+                                btnController: formVehiculoCntrl.btnCntlVehicle,
+                                title: 'Continuar'),
+                            SizedBox(height: Get.mediaQuery.size.height * 0.02),
+                          ],
                         ),
-                      ),
-                      SizedBox(height: Get.mediaQuery.size.height * 0.12),
-                      BtnLoad(
-                          onTap: () => formVehiculoCntrl.setDataVehicle(),
-                          btnController: formVehiculoCntrl.btnCntlVehicle,
-                          title: 'Continuar'),
-                      SizedBox(height: Get.mediaQuery.size.height * 0.02),
-                    ],
-                  )),
+                    ),
+                  ]
+                )),
           ),
         ),
       ),
