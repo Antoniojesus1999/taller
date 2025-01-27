4b47a263634cfc49ec0214ae2961f3e36d1d9446
Antonio Jesús Ponce Vela <antoniojesuspv099@gmail.com>
2025-01-28 00:51:58 +0100

    configurando binding formdanyos

--- lib/app/bindings/form_danyos_bindiing.dart
+++ lib/app/bindings/form_danyos_bindiing.dart
@@ -2,15 +2,11 @@ import 'package:get/get.dart';
 import 'package:taller/app/services/cliente_service.dart';
 
 import '../controllers/reparacion/form_danyos_cntrl.dart';
-import '../repositories/client_repository.dart';
 
 class ImageWithMarkersBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<ClientRepository>(() => ClientRepository());
-    Get.lazyPut(() => ClientService(clientRepository: Get.find()));
     Get.lazyPut<FormDanyosCntrl>(() => FormDanyosCntrl(
-      reparacionService: Get.find(),
-    ));
+        reparacionService: Get.find()));
   }
 }
